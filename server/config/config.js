cdf5dc06fd3bad1404dc0be4861e124ef99828cf
Jesus <bustasirio@gmail.com>
2020-02-23 20:22:19 -0600

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = "mongodb+srv://KmZSirio:ETHCbGhHvA15IbgA@cluster0-s44ng.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
